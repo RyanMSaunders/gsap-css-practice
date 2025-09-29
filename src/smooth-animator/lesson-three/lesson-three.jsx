edfcc1c95ed1c92a0b3941384316055986cec9bb
RyanMSaunders <ryan.s.maguire@gmail.com>
2025-09-29 17:24:55 -0400

    animate square

--- src/smooth-animator/lesson-three/lesson-three.jsx
+++ src/smooth-animator/lesson-three/lesson-three.jsx
@@ -1,64 +1,57 @@
 
 import gsap from "gsap";
+window.gsap = gsap; // used to export video
+
 import { useGSAP } from "@gsap/react";
 import styles from "./lesson-three.module.css";
 import { Physics2DPlugin } from "../../../gsap-public/src/Physics2DPlugin";
+import { CSSPlugin } from "../../../gsap-public/src/CSSPlugin";
+
+gsap.registerPlugin(CSSPlugin); 
+
+CSSPlugin.defaultSmoothOrigin = true;
 
 
-gsap.registerPlugin(Physics2DPlugin); 
+// gsap.registerPlugin(Physics2DPlugin); 
 
 
 const LessonThree = () => {
-  // const tl = gsap.timeline({repeat: -1, yoyo: true});
-
-  // useGSAP(() => {
-
-  //  tl.to(`.${styles.square}`, {
-  //    rotation: 90,
-  //    transformOrigin: "right bottom",
-  //    duration: 3, 
-  //   //  repeat: -1, 
-  //   //  yoyo: true, 
-  //  });
-  // //  tl.set(`.${styles.square}`, {
-  // //   x: "500px"
-  // //  });
-  //  tl.to(`.${styles.square}`, {
-  //    rotation: 180,
-  //    transformOrigin: "left bottom",
-  //    duration: 3, 
-  //   //  repeat: -1, 
-  //   //  yoyo: true, 
-  //  });
-   
-  // });
+  
   useGSAP(() => {
-  // const sq = document.querySelector(`.${styles.square}`);
-  // const rect = sq.getBoundingClientRect();
-  // const step = Math.round(rect.width); // distance to move each 90deg
-
-  // gsap.set(sq, { rotation: 0, x: 0, transformOrigin: "center center", willChange: "transform" });
-
-  // const tl = gsap.timeline({ repeat: -1 });
-  // for (let i = 0; i < 4; i++) {
-  //   tl.to(sq, {
-  //     rotation: "+=90",
-  //     x: `+=${step}`,
-  //     duration: 0.6,
-  //     ease: "power1.inOut"
-  //   });
-  // }
-  gsap.to(`.${styles.square}`, {
-  duration: 5,
-  rotation: "+=90",
-  physics2D: {
-    velocity: 200,   // forward speed in px/sec
-    angle: 0,        // 0° = move right, 90° = move down
-    angularVelocity: 90 // deg/sec rotation speed
-  },
-  repeat: -1,
-  ease: "none"
-});
+    const tl = gsap.timeline({ repeat: -1, yoyo: true })
+  
+  tl.to(`.${styles.rect}`, {
+    duration: 2,
+    rotation: "+=90",
+    smoothOrigin: true,
+    transformOrigin: "right bottom",
+    // repeat: -1,
+    ease: "power1.inOut"
+  })
+  .to(`.${styles.rect}`, {
+    duration: 2,
+    rotation: "+=90",
+    // smoothOrigin: true,
+    transformOrigin: "right top",
+    // repeat: -1,
+    ease: "power1.inOut"
+  })
+  .to(`.${styles.rect}`, {
+    duration: 2,
+    rotation: "+=90",
+    // smoothOrigin: true,
+    transformOrigin: "left top",
+    // repeat: -1,
+    ease: "power1.inOut"
+  })
+  .to(`.${styles.rect}`, {
+    duration: 2,
+    rotation: "+=90",
+    // smoothOrigin: true,
+    transformOrigin: "left bottom",
+    // repeat: -1,
+    ease: "power1.inOut"
+  });
 });
 
  
@@ -73,10 +66,41 @@ const LessonThree = () => {
           <div className={`${styles.atom} ${styles.ml}`}></div>
           <div className={`${styles.atom} ${styles.mc}`}>
 
-            <div className={styles.square}>
+          {/* <div className={styles.svgContainer}> */}
+           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 200" className={styles.svg} preserveAspectRatio="xMidYMid meet">
+              <defs>
+                <linearGradient id="rollingSquareGradient" x1="0%" y1="0%" x2="100%" y2="100%">
+                  <stop offset="0%" stopColor="#FF5B38" />
+                  <stop offset="100%" stopColor="#FFFF00" />
+                </linearGradient>
+              </defs>
+              <rect
+                className={styles.rect}
+                x="10"
+                y="100"
+                width="100"
+                height="100"
+                fill="url(#rollingSquareGradient)" 
+                stroke="rgb(0, 0, 0)"
+                strokeWidth="4"
+              />
+              {/* <rect
+                className={styles.rect}
+                x="0"
+                y="0"
+                width="200"
+                height="200"
+                style={{
+                  fill: "rgb(216, 216, 216)",
+                  stroke: "rgb(0, 0, 0)",
+                  strokeWidth: "4"
+                }}
+              /> */}
+            </svg>
+
               
             </div>
-          </div>
+          {/* </div> */}
           <div className={`${styles.atom} ${styles.mr}`}></div>
         </article>
         <article className={`${styles.molecule} ${styles.bottom}`}>
